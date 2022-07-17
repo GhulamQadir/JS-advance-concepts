6ebf4f76f22985f56414857f8b30340815cbafc9
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-07-17 05:26:09 -0700

    local storage practise

--- app.js
+++ app.js
@@ -2,36 +2,61 @@ var div = document.getElementById('testing')
 
 var localArray = [];
 function saveItem() {
+
     var getInputVal = document.getElementById('save_item')
-    localArray.push({ "name": getInputVal.value })
+    if (getInputVal.value === "") {
+        console.log("Please enter some value")
+    }
+    else {
+        localArray.push({ "name": getInputVal.value })
 
-    setItem()
+        setItem()
 
 
-    var para = document.createElement('p')
-    var paraText = document.createTextNode(getInputVal.value)
-    para.appendChild(paraText)
-    div.appendChild(para)
+        var para = document.createElement('p')
+        var paraText = document.createTextNode(getInputVal.value)
+        para.appendChild(paraText)
+        var delBtn = document.createElement('button')
+        delBtn.setAttribute('onclick', 'deleteItem(this)')
+        var delBtnText = document.createTextNode('Delete')
+        delBtn.appendChild(delBtnText)
+        para.appendChild(delBtn)
+        div.appendChild(para)
 
-    getInputVal.value = ""
+        getInputVal.value = ""
+    }
 }
 
 function setItem() {
     localStorage.setItem('names', JSON.stringify(localArray))
 }
+function deleteItem(e) {
+    e.parentNode.remove(this)
+    localArray.splice(e, 1)
+    console.log(localArray)
+    localStorage.setItem('names', JSON.stringify(localArray));
+}
 
 function localItems() {
 
-
     for (var i in localArray) {
-        var array = localArray[i]
-        var para = document.createElement('p')
-        var paraText = document.createTextNode(array.name)
-        para.appendChild(paraText)
-        div.appendChild(para)
-    }
+        if (localArray[i].name != "") {
 
+            var array = localArray[i]
+            var para = document.createElement('p')
+            var paraText = document.createTextNode(array.name)
+            var delBtn = document.createElement('button')
+            delBtn.setAttribute('onclick', 'deleteItem(this)')
+            var delBtnText = document.createTextNode('Delete')
+            delBtn.appendChild(delBtnText)
+            para.appendChild(paraText)
+            para.appendChild(delBtn)
+            div.appendChild(para)
+
+        }
+    }
 }
+
 
 function getItem() {
     var namesData = JSON.parse(localStorage.getItem('names'))
@@ -41,4 +66,4 @@ function getItem() {
     }
 }
 getItem()
-saveItem()
+// saveItem()
