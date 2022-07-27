a9c592c8889c411baa22c8cb9259dea60651276d
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-07-27 00:12:46 -0700

    fetched data from json api

--- promise.js
+++ promise.js
@@ -1,40 +1,66 @@
 // Promise
 
 var getSpinner = document.getElementById('spinner')
-
-var learningPromise = new Promise(function (resolve, reject) {
-
-    getSpinner.style.display = "block"
-
-
-    setTimeout(function () {
-        let getData = "hasData"
-
-        if (getData === "hasData") {
-            resolve("Data agya")
-            getSpinner.style.display = "none"
-
-        }
-        else {
-            reject("Data nhi aya")
-            getSpinner.style.display = "none"
-
-        }
-    }, 2000)
-
-})
-console.log(learningPromise)
-
-
-
 var promiseResult = document.getElementById('promise_result')
-learningPromise.then(function (data) {
-    console.log(`Resolve==> ${data}`)
-    promiseResult.innerHTML = data
 
 
-})
-learningPromise.catch(function (error) {
-    console.log(`Reject ==> ${error}`)
-    promiseResult.innerHTML = error
-})
+// let learningPromise = new Promise(function (resolve, reject) {
+
+//     getSpinner.style.display = "block"
+
+
+//     setTimeout(function () {
+//         const getData = "hasData"
+
+//         if (getData === "hasData") {
+//             resolve("Data fetched successfuly")
+//             getSpinner.style.display = "none"
+
+//         }
+//         else {
+//             reject("Something went wrong")
+//             getSpinner.style.display = "none"
+
+//         }
+//     }, 2000)
+
+// })
+// console.log(learningPromise)
+
+
+
+// learningPromise.then(function (data) {
+//     console.log(`Resolve==> ${data}`)
+//     promiseResult.innerHTML = data
+
+
+// })
+// learningPromise.catch(function (error) {
+//     console.log(`Reject ==> ${error}`)
+//     promiseResult.innerHTML = error
+// })
+
+
+
+
+// fetched data from json placeholder api
+function getDataFromApi() {
+    getSpinner.style.display = "block"
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+
+        .then(function (result) {
+            getSpinner.style.display = "none"
+            console.log("result==>", result)
+
+            promiseResult.innerHTML = `${result.title}<br> id: ${result.id}`
+        })
+
+        .catch(function (error) {
+            console.log(error)
+            getSpinner.style.display = "none"
+
+            promiseResult.innerHTML = error.message
+        })
+}
+getDataFromApi()
