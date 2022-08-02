20ff5d9fa19f955399e0fe86622316357e8c2888
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-02 01:14:41 -0700

    array functions

--- functions.js
+++ functions.js
@@ -1,38 +1,57 @@
 // Function Expressions (assigning function in a variable)
-let funcExpression = function () {
-    console.log("Hello World!")
-}
-funcExpression()
+// let funcExpression = function () {
+//     console.log("Hello World!")
+// }
+// funcExpression()
 
 
 
 // Arrow functions (if we have only one parameter then we dont need to make round() brackets)
-var arrorFunc = (name) => {
-    console.log(`Hi, ${name}`)
-}
-arrorFunc("Ghulam Qadir")
+// var arrorFunc = (name) => {
+//     console.log(`Hi, ${name}`)
+// }
+// arrorFunc("Ghulam Qadir")
 
 
-// more short arrow function 
-shortFunction = (a, b) => (console.log(a + b))
+// // more short arrow function
+// shortFunction = (a, b) => (console.log(a + b))
 
-shortFunction(5, 2)
+// shortFunction(5, 2)
 
 
 
 
 
 // CallBack Function
+// renderData = (name) => {
+//     console.log(name)
+// }
+
+// getData = (renderData) => {
+//     setTimeout(() => {
+//         renderData("Ghulam Qadir")
+//     }, 3000)
+// }
+// getData(renderData)
 
 
-renderData = (name) => {
-    console.log(name)
+
+///////////////////////////// ARRAY FUNCTIONS //////////////////////////////////
+// 1: Filter
+let arr = [{ name: "Ghulam Qadir", age: 17 }, { name: "Noman", age: 14 }]
+
+let filter = arr.filter(a => {
+    return a.name.includes('man')
+})
+
+console.log(filter)
+
+
+
+
+// Search
+let userName = "ghulam qadir"
+let search = "g"
+if (userName.startsWith(search)) {
+    console.log(userName)
 }
-
-getData = (renderData) => {
-    setTimeout(() => {
-        renderData("Ghulam Qadir")
-    }, 3000)
-}
-getData(renderData)
-
